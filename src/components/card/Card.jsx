93b3f9cb83d5991824f64226a8b3a3de5ef20b65
edgar <harove@gmail.com>
2019-11-12 12:48:23 -0300

    Reagieren von Karten unter Verwendung von Daten von https://via.placeholder.com/150

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,21 +1,47 @@
 import React, {Component} from 'react';
-import {Card as CardRS, CardBody, CardTitle, Container, Col, Button} from 'reactstrap';
+import {Card as CardRS, CardBody,CardImg,CardTitle,CardSubtitle, Container, Col, Button} from 'reactstrap';
+import {users} from '../../utils/mock-data'
+
 
 class Card extends Component {
-    render(){
-        
+    constructor(props){
+        super(props);
+        this.state = {
+            users: users
+        }
+    }
 
+    handleOnClick(userId){
+        return( () => {
+            this.state.users[userId-1].alive = !this.state.users[userId-1].alive;
+            this.setState(
+                users,
+            )
+        }
+        )
+    }
+
+
+
+    render(){
         return(
                 <Container>
-                    <Col lg = "6">
-                        <CardRS>
-                            <CardBody>
-                                <CardTitle>
-                                    Hey
-                                </CardTitle>
-                                <Button>Button</Button>
-                            </CardBody>
-                        </CardRS>
+                    <Col lg = "3">
+                        { this.state.users.map(usr => (
+                            <CardRS body inverse color = {usr.alive? 'success' : 'danger'} key = {usr.id}>
+                                <CardBody>
+                                    <CardImg top src= {usr.img} />
+                                    <CardTitle>
+                                        {usr.name}
+                                    </CardTitle>
+                                    <CardSubtitle>
+                                        {usr.alive? 'alive' : 'notAlive'}
+                                    </CardSubtitle>
+                                    <Button onClick = {this.handleOnClick(usr.id)} >{usr.alive? 'kill' : 'revive'}</Button>
+                                </CardBody>
+                            </CardRS>
+                        ))
+                        }
                     </Col>
                 </Container>
         )
--- src/utils/mock-data.js
+++ src/utils/mock-data.js
@@ -1,11 +1,11 @@
 export const users = [
-    {id: 1, name: 'Aragorn', age: 100, alive: true },
-    {id: 2, name: 'Isildur', age: 100, alive: true },
-    {id: 3, name: 'Legolas', age: 346, alive: true },
-    {id: 4, name: 'Beorn', age: 34566, alive: true },
-    {id: 5, name: 'Gandalf', age: 5646, alive: true },
-    {id: 6, name: 'Sauron', age: 455, alive: true },
-    {id: 7, name: 'Bilbo', age: 45, alive: false },
-    {id: 8, name: 'Frodo', age: 100, alive: false },
+    {id: 1, name: 'Aragorn', age: 100, alive: true, img: 'https://via.placeholder.com/150' },
+    {id: 2, name: 'Isildur', age: 100, alive: true, img: 'https://via.placeholder.com/151'  },
+    {id: 3, name: 'Legolas', age: 346, alive: true, img: 'https://via.placeholder.com/152' },
+    {id: 4, name: 'Beorn', age: 34566, alive: true, img: 'https://via.placeholder.com/153' },
+    {id: 5, name: 'Gandalf', age: 5646, alive: true, img: 'https://via.placeholder.com/154' },
+    {id: 6, name: 'Sauron', age: 455, alive: true, img: 'https://via.placeholder.com/155' },
+    {id: 7, name: 'Bilbo', age: 45, alive: false, img: 'https://via.placeholder.com/156' },
+    {id: 8, name: 'Frodo', age: 100, alive: false, img: 'https://via.placeholder.com/157' },
 ];
 
